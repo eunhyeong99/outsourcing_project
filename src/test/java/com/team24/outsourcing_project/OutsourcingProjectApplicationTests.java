574dbe15a9583ddab956a091b88b7f62150f1c85
eunhyeong99 <132648987+eunhyeong99@users.noreply.github.com>
2024-09-19 10:45:54 +0900

    first commit

---
+++ src/test/java/com/team24/outsourcing_project/OutsourcingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team24.outsourcing_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutsourcingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
