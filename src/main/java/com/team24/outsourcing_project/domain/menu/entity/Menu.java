ab2a506243c285f7a8bae25776da6f70b215a1a8
eunhyeong99 <132648987+eunhyeong99@users.noreply.github.com>
2024-09-19 22:35:17 +0900

    .

--- src/main/java/com/team24/outsourcing_project/domain/menu/entity/Menu.java
+++ src/main/java/com/team24/outsourcing_project/domain/menu/entity/Menu.java
@@ -1,6 +1,7 @@
 package com.team24.outsourcing_project.domain.menu.entity;
 
 import com.team24.outsourcing_project.domain.common.entity.Timestamped;
+import com.team24.outsourcing_project.domain.store.entity.Store;
 import jakarta.persistence.*;
 import lombok.NoArgsConstructor;
 
@@ -8,7 +9,18 @@ import lombok.NoArgsConstructor;
 @Table(name = "menus")
 @NoArgsConstructor
 public class Menu extends Timestamped {
+
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true,nullable = false)
+    private String menuName;
+
+    @Column(nullable = false)
+    private int menuPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id")
+    private Store store;
 
 }
