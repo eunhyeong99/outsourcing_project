aa80722fe5399f343f5955d797ce5b6a441f8c7d
pookyspooky <union0802@naver.com>
2024-09-23 11:43:26 +0900

    refactor: Store Entity 수정

--- src/main/java/com/team24/outsourcing_project/domain/store/entity/Store.java
+++ src/main/java/com/team24/outsourcing_project/domain/store/entity/Store.java
@@ -6,6 +6,7 @@ import com.team24.outsourcing_project.domain.order.entity.Order;
 import com.team24.outsourcing_project.domain.user.entity.User;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.sql.Time;
@@ -15,6 +16,7 @@ import java.util.List;
 @Table(name = "stores")
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Store extends Timestamped {
 
   @Id
@@ -26,6 +28,9 @@ public class Store extends Timestamped {
 
   @Column(nullable = false)
   private int minOrderPrice;
+
+  @Column(nullable = false)
+  private boolean isOwner;
 
   @Enumerated(EnumType.STRING)
   private StoreStatus role;
@@ -40,21 +45,41 @@ public class Store extends Timestamped {
   @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
+  @OneToMany(mappedBy = "store")
+  private List<Order> orderList;
 
-  private Store(final String name, final int minOrderPrice, final StoreStatus role,
-      final Time openTime, final Time closeTime, final User user) {
+  @OneToMany(mappedBy = "store")
+  private List<Menu> menuList;
+
+
+  private Store(final String name, final int minOrderPrice, final boolean isOwner,
+      final StoreStatus role, final Time openTime, final Time closeTime,
+      final User user) {
     this.name = name;
     this.minOrderPrice = minOrderPrice;
+    this.isOwner = isOwner;
     this.role = role;
     this.openTime = openTime;
     this.closeTime = closeTime;
     this.user = user;
+
   }
 
-  private static Store createStore(final String name, final int minOrderPrice,
-      final StoreStatus role, final Time openTime, final Time closeTime, final User user) {
-    return new Store(name, minOrderPrice, role, openTime, closeTime, user);
+  public static Store createStore(final String name, final int minOrderPrice, final boolean isOwner,
+      final StoreStatus role, final Time openTime, final Time closeTime,
+      final User user) {
+    return new Store(name, minOrderPrice, isOwner, role, openTime, closeTime, user);
   }
 
+  public void update(String name, int minOrderPrice, Time openTime, Time closeTime) {
+    this.name = name;
+    this.minOrderPrice = minOrderPrice;
+    this.openTime = openTime;
+    this.closeTime = closeTime;
 
+  }
+
+  public void changeStatus(final StoreStatus newStatus) {
+    this.role = newStatus;
+  }
 }
