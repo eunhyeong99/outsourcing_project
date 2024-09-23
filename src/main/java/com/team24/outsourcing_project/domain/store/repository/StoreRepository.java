dc95ed60d75099ade9499add1219024a441bc2b2
MinJun Song <j34679@naver.com>
2024-09-23 11:03:12 +0900

    add repository

--- src/main/java/com/team24/outsourcing_project/domain/store/repository/StoreRepository.java
+++ src/main/java/com/team24/outsourcing_project/domain/store/repository/StoreRepository.java
@@ -4,10 +4,12 @@ import com.team24.outsourcing_project.domain.store.entity.Store;
 import com.team24.outsourcing_project.domain.store.entity.StoreStatus;
 import com.team24.outsourcing_project.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import javax.management.relation.Role;
 import java.util.List;
 
+@Repository
 public interface StoreRepository extends JpaRepository<Store,Long> {
 
     Store findByIdAndRole(Long id,StoreStatus role);
