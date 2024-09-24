0d78cb980c12464b4db5615ba796c4a913290b02
bronbe <bronsta111@gmail.com>
2024-09-24 13:09:11 +0900

    feat(review): 리뷰 생성, 조회 기능 구현

--- src/main/java/com/team24/outsourcing_project/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/team24/outsourcing_project/domain/review/repository/ReviewRepository.java
@@ -1,7 +1,24 @@
 package com.team24.outsourcing_project.domain.review.repository;
 
 import com.team24.outsourcing_project.domain.review.entity.Review;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review,Long>{
+
+    List<Review> findAllByOrderId(final long orderId);
+
+    boolean existsByUserIdAndOrderId(final long userId, final long orderId);
+
+    @Query("SELECT r " +
+            "FROM Review r " +
+            "LEFT JOIN r.order o " +
+            "WHERE o.store.id = :storeId AND r.score BETWEEN :lowerBound AND :upperBound " +
+            "ORDER BY r.createdAt DESC")
+    List<Review> findAllByStoreId(
+            @Param("storeId") final long storeId,
+            @Param("lowerBound") final int lowerBound,
+            @Param("upperBound") final int upperBound);
 }
