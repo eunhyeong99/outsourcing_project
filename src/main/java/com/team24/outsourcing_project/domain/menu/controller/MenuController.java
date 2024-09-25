2e5426a28d8cb5b1fc389a757e5c05a4288597b2
bronbe <bronsta111@gmail.com>
2024-09-25 12:58:04 +0900

    refactor(menu): ResponseStatus 제거

--- src/main/java/com/team24/outsourcing_project/domain/menu/controller/MenuController.java
+++ src/main/java/com/team24/outsourcing_project/domain/menu/controller/MenuController.java
@@ -1,13 +1,15 @@
 package com.team24.outsourcing_project.domain.menu.controller;
 
 import com.team24.outsourcing_project.domain.menu.dto.MenuRequestDto;
-import com.team24.outsourcing_project.domain.menu.dto.MenuResponseDto;
 import com.team24.outsourcing_project.domain.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.awt.*;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,36 +18,17 @@ public class MenuController {
     private final MenuService menuService;
 
     @PostMapping("/menus")
-    public String createMenus(@RequestBody MenuRequestDto menuRequestDto) {
-
+    public void createMenus(@RequestBody MenuRequestDto menuRequestDto) {
         menuService.createMenus(menuRequestDto);
-
-        return "생성 완료.";
     }
 
     @PutMapping("/menus/{id}")
-    public String updateMenus(@RequestBody MenuRequestDto menuRequestDto, @PathVariable Long id) {
-
+    public void updateMenus(@RequestBody MenuRequestDto menuRequestDto, @PathVariable Long id) {
         menuService.updateMenus(menuRequestDto,id);
-
-        return "수정 완료.";
     }
 
-//    @GetMapping("/menus/{storeId}")
-//    public List<MenuResponseDto> getMenus(@PathVariable(name = "storeId") Long storeId) {
-//
-//        // Fetch the menu list from the service
-//        List<MenuResponseDto> menuResponseDtos = menuService.getMenus(storeId);
-//
-//        // Return the list of MenuResponseDto
-//        return menuResponseDtos;
-//    }
-
     @DeleteMapping("/menus/{id}")
-    public String deleteMenus(@PathVariable Long id) {
-
+    public void deleteMenus(@PathVariable Long id) {
         menuService.deleteMenus(id);
-
-        return "삭제 완료.";
     }
 }
